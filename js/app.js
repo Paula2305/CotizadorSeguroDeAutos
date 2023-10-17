7420e0d3b1a2f33bcb845f1950f0c0e4b036a517
Paula2305 <polimoreno1976@gmail.com>
2023-10-16 21:05:02 -0300

    Validación del formulario

--- js/app.js
+++ js/app.js
@@ -34,6 +34,36 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 
+eventListeners();
+function eventListeners(){
+    const formulario = document.querySelector('#cotizar-seguro');
+    formulario.addEventListener('submit', cotizarSeguro);
+}
+
+function cotizarSeguro(e){
+    e.preventDefault();
+
+    // Leer la marca seleccionada
+    const marca = document.querySelector('#marca').value;
+    
+    // Leer el año seleccionado
+    const year = document.querySelector('#year').value;
+    
+    
+    // Leer el tipo de cobertura
+    const tipo = document.querySelector('input[name="tipo"]:checked').value;
+    console.log(tipo);
+
+    if(marca === '' || year === '' || tipo === ''){
+        console.log('No paso la validacion');
+    }else{
+        console.log('Si paso la validacion');
+
+    }
+
+}
+
+
 
 
 
